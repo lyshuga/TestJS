043546ffee63552356087f4ff8a9d9e0223631ea
lyshuga <kolia.liashuga@gmail.com>
2017-07-21 14:06:51 +0300

    Small replacements

--- G/G/Startup.cs
+++ G/G/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace G
-{ //start
+{ //s
     public class Startup
     {
         public Startup(IHostingEnvironment env)
